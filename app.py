d022533b86af71b7637522d7e2b20b3958089a37
thisishwan2 <feel000617@gmail.com>
2023-09-05 16:41:51 +0900

    [FIX] 증강 코드 변경

--- app.py
+++ app.py
@@ -6,9 +6,14 @@ import urllib
 import zipfile
 from concurrent.futures import ThreadPoolExecutor
 from threading import Thread
+import numpy as np
 
 from PIL import Image
 from torch.utils.data import DataLoader, Dataset
+import albumentations as albu
+from albumentations.pytorch import ToTensorV2
+from torchvision.transforms import ToTensor, ToPILImage
+from torchvision.utils import save_image
 
 import torch
 import torch.nn as nn
@@ -24,18 +29,12 @@ from config import AWS_S3_BUCKET_NAME, AWS_S3_BUCKET_REGION
 
 # mysql = MySQL()
 app = Flask(__name__)
-# app.config['MYSQL_USER'] = config.user
-# app.config['MYSQL_PASSWORD'] = config.password
-# app.config['MYSQL_DB'] = config.db
-# app.config['MYSQL_HOST'] = config.host
-# mysql.init_app(app)
 
 db = pymysql.connect(host=config.host, user=config.user, password=config.password, db=config.db, charset=config.charset) # 별도 컨피그에 정리
-# cursor = db.cursor()
 executor = ThreadPoolExecutor(max_workers=10)
 
 
-class CustomDataset(Dataset):
+class CustomDataset_train(Dataset):
     def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
         self.transform = transform
@@ -50,6 +49,29 @@ class CustomDataset(Dataset):
 
         if self.transform:
             image = self.transform(image)
+
+        return image
+
+class CustomDataset_image_extract(Dataset):
+    def __init__(self, root_dir, max_saved=30):
+        self.root_dir = root_dir
+        self.transform = get_training_augmentation()
+        self.allowed_extensions = ['.jpg', '.jpeg', '.png']
+        self.image_files = [f for f in os.listdir(root_dir) if os.path.splitext(f)[1].lower() in self.allowed_extensions]
+        self.max_saved = max_saved
+
+    def __len__(self):
+        return min(self.max_saved, len(self.image_files))
+
+    def __getitem__(self, idx):
+        img_name = os.path.join(self.root_dir, self.image_files[idx])
+        image = Image.open(img_name).convert("RGB")
+
+        if self.transform:
+            image = np.array(image)  # NumPy 배열로 변환
+            augmented = self.transform(image=image)
+            image = augmented['image']
+            image = ToTensorV2()(image=image)["image"]  # PyTorch 텐서로 다시 변환
 
         return image
 
@@ -75,15 +97,15 @@ def s3_connection():
 s3 = s3_connection()
 
 
-def init():
-    t1 = Thread(target=make_zip)  # make_zip 함수를 실행할 스레드 생성
-    t1.daemon = True  # 데몬 스레드로 설정
-    t1.start()  # 스레드 시작
-
-# @app.route("/generate", methods=['POST'])
-# def handle_request():
-#     init()  # init 함수 호출로 스레드를 시작
-#     return jsonify({"status": "processing"}), 202
+# def init():
+#     t1 = Thread(target=make_zip)  # make_zip 함수를 실행할 스레드 생성
+#     t1.daemon = True  # 데몬 스레드로 설정
+#     t1.start()  # 스레드 시작
+#
+# # @app.route("/generate", methods=['POST'])
+# # def handle_request():
+# #     init()  # init 함수 호출로 스레드를 시작
+# #     return jsonify({"status": "processing"}), 202
 
 
 @app.route("/ganerate", methods = ['POST'])
@@ -104,20 +126,17 @@ def make_zip(): # 멀티 프로세싱 처리
             try:
                 # S3로 부터 객체 가져오기
                 obj = s3.get_object(Bucket=AWS_S3_BUCKET_NAME, Key=upload_file_name)
-                zip_data = obj['Body'].read() # 바이트 형태의 zip 파일
+                zip_data = obj['Body'].read()
 
                 # 로컬에 압축 해제할 디렉토리 경로 설정 (예: 데스크탑)
                 desktop_path = os.path.expanduser("~/Desktop") #추후 ec2에 맞게 변경
-                # 로컬에 zip파일을 저장할 경로 설정
+                # /Desktop/abc.zip
                 zip_file_path = os.path.join(desktop_path, upload_file_name) #추후 이름도 변경
 
-                #확장자 제거한 파일 이름 -> 폴더 명으로 사용
-                base_name = os.path.splitext(upload_file_name)[0]
+                #확장자 제거한 파일 이름 -> 폴더 명으로 사용 (abc)
+                new_folder_name = os.path.splitext(upload_file_name)[0]
 
-                #새로 만들 폴더 이름
-                new_folder_name = base_name
-
-                # 새로 생성할 폴더 경로
+                # /Desktop/abc
                 new_folder_path = os.path.join(desktop_path, new_folder_name)
 
                 # 지원할 이미지 확장자 리스트
@@ -129,6 +148,7 @@ def make_zip(): # 멀티 프로세싱 처리
                         zip_file.write(zip_data)
                     print("ZIP 파일이 로컬에 저장되었습니다.")
 
+                    # /Desktop/abc 생성
                     os.makedirs(new_folder_path)
 
                     # 로컬에 저장된 ZIP 파일을 압축 해제
@@ -136,23 +156,8 @@ def make_zip(): # 멀티 프로세싱 처리
                         zip_ref.extractall(new_folder_path)
                     print("압축 해제가 완료되었습니다.")
 
-                    # 디렉토리 내부의 파일 및 폴더 검사 및 삭제
-                    for root, dirs, files in os.walk(new_folder_path):
-                        for name in files + dirs:
-                            file_path = os.path.join(root, name)
-                            extension = os.path.splitext(name)[1].lower()
-
-                            # 파일 및 폴더 이름을 CP437로 인코딩 후 UTF-8로 디코딩
-                            decoded_filename = name.encode('cp437').decode('utf-8')
-
-                            # 지원하는 확장자가 아닌 경우 파일 또는 폴더 삭제
-                            if extension not in supported_extensions:
-                                if os.path.isfile(file_path):
-                                    os.remove(file_path)
-                                    print(f"삭제된 파일: {decoded_filename}")
-                                elif os.path.isdir(file_path):
-                                    os.rmdir(file_path)
-                                    print(f"삭제된 폴더: {decoded_filename}")
+                    # 디렉터리 내부 검사 및 확장자가 맞지 않는 파일 삭제
+                    directory_search_and_delete_file(new_folder_path, supported_extensions)
 
                     # 로컬에 저장된 ZIP 파일 삭제
                     os.remove(zip_file_path)
@@ -161,79 +166,84 @@ def make_zip(): # 멀티 프로세싱 처리
                 except Exception as e:
                     print("압축 해제 및 파일 삭제 중 오류 발생:", e)
 
-                # 압축해제한 zip 파일 갯수 확인
+                # 압축해제 한 파일 갯수 확인
                 files_and_folders = os.listdir(new_folder_path)
                 # 파일만 걸러내어 그 수를 반환합니다.
                 print(len([name for name in files_and_folders if os.path.isfile(os.path.join(new_folder_path, name))]))
 
                 # 데이터 증강
-                data_transforms = transforms.Compose([
+                # data_transforms = transforms.Compose([
+                #
+                #     transforms.RandomHorizontalFlip(),  # 수평뒤집기
+                #     transforms.RandomRotation(10),  # 10도 내에서 회전
+                #     transforms.RandomAffine(0, translate=(0.2, 0.2)),  # 20퍼센트 좌우 및 상하 이동
+                #     transforms.RandomResizedCrop(128, scale=(0.8, 1.0)),  # 무작위로 이미지 크롭 및 128x128 사이즈로 이미지 설정
+                # ])
 
-                    transforms.RandomHorizontalFlip(),  # 수평뒤집기
-                    transforms.RandomRotation(10),  # 10도 내에서 회전
-                    transforms.RandomAffine(0, translate=(0.2, 0.2)),  # 20퍼센트 좌우 및 상하 이동
-                    transforms.RandomResizedCrop(128, scale=(0.8, 1.0)),  # 무작위로 이미지 크롭 및 128x128 사이즈로 이미지 설정
-                ])
+                # 증강된 이미지 저장 경로 /Desktop/abc/OUTPUT
+                output_path = os.path.join(new_folder_path, "OUTPUT")
 
-                output_path = os.path.join(new_folder_path, "OUTPUT")  # 증강된 이미지 저장 경로
-                print(output_path)
                 if not os.path.exists(output_path):
                     os.mkdir(output_path)
 
-                print(new_folder_path)
-                dataset = CustomDataset(root_dir=new_folder_path, transform=data_transforms)
-                # dataset = datasets.ImageFolder(new_folder_path)
+                # 데이터셋 및 DataLoader 설정
+                dataset = CustomDataset_image_extract(new_folder_path)
+                print("Number of data samples in the dataset:", len(dataset))
+                data_loader = DataLoader(dataset, batch_size=len(dataset), shuffle=True)
+
+                # 저장할 이미지 수 설정
+                max_saved = 10 # 총 저장할 이미지 수(마지막에 배포시 10000장으로)
+
+                # 입력 이미지의 개수 확인
                 num_images = len(dataset)
 
-                print(len(dataset))
-
-                max_saved = 10  # 총 저장할 이미지 수(마지막에 배포시 10000장으로)
-
+                # 이미지 저장 카운터
+                saved_count = 0
 
                 ## 데이터 증강
                 if num_images >= max_saved:
-                    # 10장 이상이면 원본을 그대로 복사-> 이거 10000장으로 할거임
+                    print("Enough images, skipping augmentation.")
                     output_path=new_folder_path
-
+                    pass
                 else:
-                    loader = torch.utils.data.DataLoader(dataset, batch_size=len(dataset), shuffle=True,
-                                                         collate_fn=my_collate)  # 모든 이미지를 한 번에 로드
-                    all_image_paths = []  # 모든 이미지를 담을 리스트 초기화
-                    # 모든 이미지를 all_images 리스트에 담기
-                    # for i, images in enumerate(loader):
-                    #     all_images.extend(images)
-                    #     # all_images.append(images)
+                    # 로더에서 모든 이미지를 로드
+                    # all_images = next(iter(data_loader))
+                    all_images = []
+                    for i in range(len(dataset)):
+                        image = dataset[i]
+                        all_images.append(image)
 
-                    for root, _, files in os.walk(new_folder_path):  # dataset_root는 이미지 파일들이 있는 디렉토리 경로입니다.
-                        for file in files:
-                            if file.endswith((".jpg", ".jpeg", ".png")):  # 이미지 파일인 경우만 고려하도록 확장자를 지정합니다.
-                                file_path = os.path.join(root, file)
-                                all_image_paths.append(file_path)
-
-                    total_saved = 1  # 총 저장된 이미지 수 초기화
-
-                    while total_saved <= max_saved:
+                    # 증강된 이미지 저장
+                    while saved_count < max_saved:
                         # 랜덤하게 하나의 이미지를 선택
-                        image = random.choice(all_image_paths)
-                        print(type(image))
+                        random_idx = random.randint(0, len(all_images) - 1)
+                        image = all_images[random_idx]
+                        image = image / 255.0 if image.max() > 1 else image  # 이미지를 0-1 범위로 정규화
 
-                        image = Image.open(image).convert("RGB")
+                        pil_image = ToPILImage()(image.cpu().squeeze(0))  # PyTorch 텐서를 PIL 이미지로 변환
+                        np_image = np.array(pil_image)  # PIL 이미지를 NumPy 배열로 변환
+                        augmented = get_training_augmentation()(image=np_image)  # 증강 적용
+                        tensor_image = ToTensor()(augmented["image"])  # NumPy 배열을 PyTorch 텐서로 변환
 
-                        # 선택한 이미지에 증강 적용
-                        transformed_image = data_transforms(image)  # PIL 이미지 변환
-                        transformed_image.save(os.path.join(output_path, f"augmented_{total_saved}.jpg"))
+                        save_image(tensor_image, os.path.join(output_path, f"augmented_image_{saved_count + 1}.jpg"))
+                        saved_count += 1
 
-                        total_saved += 1
+                    # all_image_paths = []
+                    # for root, _, files in os.walk(new_folder_path):  # dataset_root는 이미지 파일들이 있는 디렉토리 경로입니다.
+                    #     for file in files:
+                    #         if file.endswith((".jpg", ".jpeg", ".png")):  # 이미지 파일인 경우만 고려하도록 확장자를 지정합니다.
+                    #             file_path = os.path.join(root, file)
+                    #             all_image_paths.append(file_path)
 
                 print("딥러닝 시작 전")
-
+                # breakpoint()
                 # 딥러닝
                 transform = transforms.Compose([
                     transforms.Resize((256, 256)),  # 이미지 크기를 조정합니다.
                     transforms.ToTensor(),  # 이미지를 텐서로 변환합니다.
                 ])
 
-                train_ds = CustomDataset(root_dir=output_path, transform=transform)
+                train_ds = CustomDataset_train(root_dir=output_path, transform=transform)
 
                 # train_ds 생성 후 출력
                 print("데이터셋 샘플 개수:", len(train_ds))
@@ -461,6 +471,27 @@ def make_zip(): # 멀티 프로세싱 처리
     finally:
         db.close()
 
+
+def directory_search_and_delete_file(new_folder_path, supported_extensions):
+    # 디렉토리 내부의 파일 및 폴더 검사 및 삭제
+    for root, dirs, files in os.walk(new_folder_path):
+        for name in files + dirs:
+            file_path = os.path.join(root, name)
+            extension = os.path.splitext(name)[1].lower()
+
+            # 파일 및 폴더 이름을 CP437로 인코딩 후 UTF-8로 디코딩
+            decoded_filename = name.encode('cp437').decode('utf-8')
+
+            # 지원하는 확장자가 아닌 경우 파일 또는 폴더 삭제
+            if extension not in supported_extensions:
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+                    print(f"삭제된 파일: {decoded_filename}")
+                elif os.path.isdir(file_path):
+                    os.rmdir(file_path)
+                    print(f"삭제된 폴더: {decoded_filename}")
+
+
 def to_device(data, device):
     """Move tensor(s) to chosen device"""
     if isinstance(data, (list,tuple)):
@@ -595,6 +626,45 @@ def zip_images(folder_path, output_zip_name):
             zipf.write(file_path, image_file)
 
     print("ZIP 파일 생성 완료.")
+
+# 데이터 증강 코드
+def get_training_augmentation():
+    train_transform = [
+
+        albu.HorizontalFlip(p=0.5), # 0.5확률로 수평 뒤집기
+        albu.ShiftScaleRotate(scale_limit=[0.95, 1.05], rotate_limit=45, shift_limit=0.005, p=1, border_mode=0 ), # 이동, 확대, 축소, 회전
+        albu.PadIfNeeded(min_height=400, min_width=400, always_apply=True, border_mode=0),# 이미지가 지정크기보다 작으면, 패딩함
+
+        albu.GaussNoise(p=0.2), # 0.2확률로 가우시안 노이즈 추가
+        albu.Perspective(p=0.3), # 0.3 확률로 투시 변환
+
+        albu.OneOf(    # 주어진 증강 중 하나를 확률로 CLANE, 랜덤 밝기, 랜덤 감마 등을 적용
+            [
+                albu.CLAHE(p=1),
+                albu.RandomBrightness(p=1),
+                albu.RandomGamma(p=1),
+            ],
+            p=0.8,
+        ),
+
+        albu.OneOf( #이미지 날카롭게, 윤곽 강조, 블러 정도 조절
+            [
+                albu.Sharpen(p=1),
+                albu.Blur(blur_limit=3, p=1),
+                albu.MotionBlur(blur_limit=3, p=1),
+            ],
+            p=0.8,
+        ),
+
+        albu.OneOf(
+            [
+                albu.RandomContrast(p=1), #대비
+                albu.HueSaturationValue(p=1), #채도
+            ],
+            p=0.8,
+        ),
+    ]
+    return albu.Compose(train_transform)
 
 if __name__ == '__main__':
     app.run(debug=False)
